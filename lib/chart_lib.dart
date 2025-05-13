e58e030ee02cd5f6b725f4d5eb6cc66436a6f7d7
SaurabhS120 <saurabh.sonar120@gmail.com>
2025-05-14 00:44:48 +0530

    Basic chart

--- lib/chart_lib.dart
+++ lib/chart_lib.dart
@@ -1,13 +1,25 @@
 library chart_lib;
 
-import 'package:flutter/widgets.dart';
+import 'package:chart_lib/chart_painter.dart';
+import 'package:flutter/material.dart';
 
-class Chart extends StatelessWidget{
+class Chart extends StatelessWidget {
   const Chart({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Chart widget");
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.grey,
+          width: 1,
+        ),
+      ),
+      height: 300,
+      width: 400,
+      child: CustomPaint(
+        painter: LineChartPainter(),
+      ),
+    );
   }
-
 }
---
+++ lib/chart_painter.dart
@@ -0,0 +1,82 @@
+
+import 'package:flutter/material.dart';
+
+class LineChartPainter extends CustomPainter {
+  static const double chartPadding = 12.0;
+  static const double horizontalGapBetweenGraph = 24.0;
+  static const double verticalGapBetweenGraph = 24.0;
+  @override
+  void paint(Canvas canvas, Size size) {
+    double height = size.height;
+    double width = size.width;
+    // Fill circle
+    final axisPainter = Paint()
+      ..color = Colors.grey
+      ..style = PaintingStyle.stroke;
+    final graphPainter = Paint()
+      ..color = Colors.black26
+      ..style = PaintingStyle.stroke;
+    final axisInterceptPoint = Offset(chartPadding, height-chartPadding);
+    final xAxisEndPoint = Offset(width-chartPadding, height-chartPadding);
+    final yAxisEndPoint = Offset(chartPadding, chartPadding);
+    // x-axis line
+    canvas.drawLine(axisInterceptPoint, xAxisEndPoint, axisPainter);
+    // y-axis line
+    canvas.drawLine(axisInterceptPoint, yAxisEndPoint, axisPainter);
+    // vertical lines
+    for(double x =chartPadding+horizontalGapBetweenGraph;x<xAxisEndPoint.dx;x+=horizontalGapBetweenGraph){
+      final verticalLineTopPoint = Offset(x, chartPadding);
+      final verticalLineBottomPoint = Offset(x, axisInterceptPoint.dy);
+      canvas.drawLine(verticalLineTopPoint, verticalLineBottomPoint, graphPainter);
+    }
+    // horizontal lines
+    for(double y =xAxisEndPoint.dy;y>chartPadding+verticalGapBetweenGraph;y-=verticalGapBetweenGraph){
+      final horizontalLineLeft = Offset(chartPadding, y);
+      final horizontalLineRight = Offset(xAxisEndPoint.dx, y);
+      canvas.drawLine(horizontalLineLeft, horizontalLineRight, graphPainter);
+    }
+    GraphLinePainter(canvas,height: height,width: width).drawGraph([
+      GraphLineCoordinates(0,0),
+      GraphLineCoordinates(100,100),
+      GraphLineCoordinates(200,250),
+      GraphLineCoordinates(300,50),
+      GraphLineCoordinates(350,300),
+    ]);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+
+}
+class GraphLinePainter{
+  final Canvas canvas;
+  final double height;
+  final double width;
+  final linePainter = Paint()
+    ..color = Colors.black
+    ..style = PaintingStyle.stroke;
+
+  GraphLinePainter(this.canvas,{required this.height, required this.width});
+
+  void _line({required double x1,required double y1,required double x2,required double y2,}){
+    final startPoint = Offset(LineChartPainter.chartPadding+x1, height-LineChartPainter.chartPadding-y1);
+    final endPoint = Offset(LineChartPainter.chartPadding+x2, height-LineChartPainter.chartPadding-y2);
+    canvas.drawLine(startPoint, endPoint, linePainter);
+  }
+
+  void drawGraph(List<GraphLineCoordinates> coordinates){
+    for(int i=0;i<coordinates.length-1;i++){
+      final startCoordinate = coordinates[i];
+      final endCoordinate = coordinates[i+1];
+      _line(x1: startCoordinate.x, y1: startCoordinate.y, x2: endCoordinate.x, y2: endCoordinate.y);
+    }
+  }
+
+}
+class GraphLineCoordinates{
+  final double x;
+  final double y;
+  const GraphLineCoordinates(this.x,this.y);
+}
